d38888b07cea3670a26f17d5c0ea029e73de7333
Mathieu <mathieu.laureti@gmail.com>
2025-11-11 09:41:28 -0500

    gitignore secret test

--- src/secrets.h
+++
@@ -1,8 +0,0 @@
-// TEST GITIGNORE
-const char *ssid = "";
-const char *password = "";
-const char *server_local_ip = "";
-const int server_port = 0;
-const char *upload_endpoint = "/upload";
-
-const char sleep_time_sec = 1800;
